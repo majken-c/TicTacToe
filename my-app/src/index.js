82ec00dc52dc5e12fee9262ccd86596832b23332
Majken Christensen <majken.christensen@1928d.com>
2020-05-22 16:35:56 +0200

    wip

---
+++ my-app/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => {alert('Du har klikket på et tal');
+      }}>
+        {this.props.nummer} 
+      </button>
+    ); // ved at tilføje {this.props.nummer} kaldes den "prop" som er sat i Board.
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(tal) {
+    return <Square nummer={tal} />; // Du kan kalde "nummer" hvad du vil og "tal" hvad du vil, så længe argumentet hedder det samme
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
